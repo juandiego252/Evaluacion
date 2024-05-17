1d45cd45e46c2c77b300b3fbdaa204ea5d26b8ae
juandiego252 <juandiegojd252@gmail.com>
2024-05-17 16:51:06 -0500

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
+
+  goToLoginPage() {
+    this.navCtrl.navigateForward('/login'); // Navegar a la página de inicio de sesión
+  }
+
+  goToSignUpPage() {
+    this.navCtrl.navigateForward('/signup'); // Navegar a la página de registro
+  }
 
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,18 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const firebaseConfig = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCGtF4FJjZKE1_7145pt4nFsW98hLi_QrI",
+    authDomain: "a-aa1b2.firebaseapp.com",
+    projectId: "a-aa1b2",
+    storageBucket: "a-a3e2b.appspot.com",
+    messagingSenderId: "340088945622",
+    appId: "1:340088945622:web:1b71dfe349b05b239c0277",
+    measurementId: "G-T048FCYN8K"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
